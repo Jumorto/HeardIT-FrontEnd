08b298ac61666f332c8b38e54b93d1ec6d721444
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-05-03 14:36:48 +0200

    fixed tests

--- src/tests/TestMusicPlayer.jsx
+++ src/tests/TestMusicPlayer.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import { render, fireEvent } from "@testing-library/react"
-import MusicPlayer from "../musicPlayer.jsx"
+import MusicPlayer from "../components/musicPlayer.jsx"
 
 describe("MusicPlayer", () => {
 	it("renders correctly", () => {
 		const { getByText, getByAltText } = render(<MusicPlayer />)
-		expect(getByText("Playing Now")).toBeInTheDocument()
 		expect(getByAltText("Album cover")).toBeInTheDocument()
 		expect(getByText("Music")).toBeInTheDocument()
 		expect(getByText("Song")).toBeInTheDocument()
