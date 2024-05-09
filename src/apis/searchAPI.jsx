b8086b6deda926356c9631e89f581fd86e5dfa9a
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-05-09 15:31:11 +0200

    Search-service connection to the GKE cluster

--- src/apis/searchAPI.jsx
+++ src/apis/searchAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8081/api/search"
+const BASE_URL = "http://34.89.218.176:8081/api/search"
 
 const SearchAPI = {
 	getAllSongs: () => axios.get(BASE_URL).then((response) => response.data),
